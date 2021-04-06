aa1705131a0d0b7aa980b0a9ac25be112eeae6af
russuran <Kolpak603@gmail.com>
2021-04-06 21:17:56 +0300

    part-save (nothing done)

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
+@app.route('/about')
+def about():
+    return render_template('about_us.html')
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
