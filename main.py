af3d860a0295425f414882b88266721be70d2dd9
russuran <Kolpak603@gmail.com>
2021-04-07 22:24:51 +0300

    not working w db

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from data import db_session
+from data.cards import Card
 
 
 app = Flask(__name__)
@@ -8,6 +9,18 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/cards.db")
+    card = Card()
+    with open("static/images/Spoon.jpg", "rb") as image:
+        f = image.read()
+
+
+    card.tite = "Spoon"
+    card.content = "Алюминиевая ложка"
+    card.price = "300"
+    card.photo = f
+    db_sess = db_session.create_session()
+    db_sess.add(card)
+    db_sess.commit()
     app.run()
 
 
